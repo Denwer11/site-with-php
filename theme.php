b0a0319468ed273238e67fb88a342498167cf692
Даша <90269475+Denwer11@users.noreply.github.com>
2021-10-23 20:07:22 +0300

    Add files via upload

---
+++ theme.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+
+if (isset($_GET["theme"])) {
+    $theme = $_GET["theme"];
+
+    if ($theme == "master" || $theme == "dark") {
+        $_SESSION["theme"] = $theme;
+    }
+}
